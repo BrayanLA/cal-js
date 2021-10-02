62e41c7029c84230fda2a6290876e42f28da013f
BrayanLA <brayan.david0@hotmail.com>
2021-10-02 17:23:21 -0500

    main

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,20 +1,21 @@
 function suma(){
-    var x = parseInt(document.getElementById('n1').value);
-    var y = parseInt(document.getElementById('n2').value);
-    alert("El resultado de tu suma es: " + (x+y));
+    var a = parseInt(document.getElementById('n1').value);
+    var b = parseInt(document.getElementById('n2').value);
+    const calculate = (element)=>{}
+    alert("El resultado de tu suma es: " + (${a} ${calculate} ${b}));
 }
 function resta(){
-    var x = parseInt(document.getElementById('n1').value);
-    var y = parseInt(document.getElementById('n2').value);
-    alert("El resultado de tu suma es: " + (x-y));
+    var a = parseInt(document.getElementById('n1').value);
+    var b = parseInt(document.getElementById('n2').value);
+    alert("El resultado de tu suma es: " + (a-b));
 }
 function multiplicacion(){
-    var x = parseInt(document.getElementById('n1').value);
-    var y = parseInt(document.getElementById('n2').value);
-    alert("El resultado de tu suma es: " + (x*y));
+    var a = parseInt(document.getElementById('n1').value);
+    var b = parseInt(document.getElementById('n2').value);
+    alert("El resultado de tu suma es: " + (a*b));
 }
 function division(){
-    var x = parseInt(document.getElementById('n1').value);
-    var y = parseInt(document.getElementById('n2').value);
-    alert("El resultado de tu suma es: " + (x/y));
+    var a = parseInt(document.getElementById('n1').value);
+    var b = parseInt(document.getElementById('n2').value);
+    alert("El resultado de tu suma es: " + (a/b));
 }
