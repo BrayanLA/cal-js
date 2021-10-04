d61ab44c4af8614322ccc71939c783c136950794
BrayanLA <brayan.david0@hotmail.com>
2021-10-04 17:52:24 -0500

    Mejoras de Javascript

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,21 +1,15 @@
-function suma(){
-    var a = parseInt(document.getElementById('n1').value);
-    var b = parseInt(document.getElementById('n2').value);
-    const calculate = (element)=>{}
-    alert("El resultado de tu suma es: " + (${a} ${calculate} ${b}));
+let firstValue = document.getElementById('n1');
+let secondValue = document.getElementById('n2');
+
+
+function prueba(operation) {
+    if (firstValue.value === `` || secondValue.value === ``){
+        alert("Aún no has ingresado ambos valores")
+    } else {
+    const operator = operation.target.textContent
+    let resultado = eval (` ${firstValue.value} ${operator} ${secondValue.value}`);
+    alert(`El resultado es ${resultado}`)
+    }
 }
-function resta(){
-    var a = parseInt(document.getElementById('n1').value);
-    var b = parseInt(document.getElementById('n2').value);
-    alert("El resultado de tu suma es: " + (a-b));
-}
-function multiplicacion(){
-    var a = parseInt(document.getElementById('n1').value);
-    var b = parseInt(document.getElementById('n2').value);
-    alert("El resultado de tu suma es: " + (a*b));
-}
-function division(){
-    var a = parseInt(document.getElementById('n1').value);
-    var b = parseInt(document.getElementById('n2').value);
-    alert("El resultado de tu suma es: " + (a/b));
-}
+ document.getElementById('btngrup').addEventListener('click', prueba);
+
